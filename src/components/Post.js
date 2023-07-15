3f1a0bf23ec598b586820040e7b8c9abb4cca907
Lokeshwar Rao Chilamcharla <lokeshwa@buffalo.edu>
2023-07-14 22:18:25 -0400

    react application that can post

---
+++ src/components/Post.js
@@ -0,0 +1,18 @@
+import classes from './Post.module.css'
+import { Link } from 'react-router-dom'
+export function Post({id, author, body}){
+    return (
+        
+          <li className={classes.post}>
+            <Link to={id}>
+              
+                    <p className={classes.author}>{author}</p>
+                    <p className={classes.text}>{body}</p>
+                
+            </Link>
+          </li>
+        
+    )
+}
+
+// export default Post;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RootLayout from './routes/RootLayout';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import NewPost, {sumbitAction} from './routes/NewPost';
+import Posts,{loader as postsLoader} from './routes/Posts';
+import PostDetails, {loader as postDetails}from './routes/PostDetails';
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <RootLayout />,
+    children: [
+      {
+        path: '/', element: <Posts />,
+        loader: postsLoader,
+        children: [
+          { path: '/create-post', element: <NewPost />, action:sumbitAction},
+          { path: '/:id', element:<PostDetails/>, loader:postDetails}
+        ]
+      },
+
+    ],
+  }
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>
 );
 
